b2b395b852be6f23e9848d450b66e58cf266e1cc
Kyurem <41705282+monoyoshi@users.noreply.github.com>
2024-06-10 14:55:52 -0500

    re-enabling the pages for hotaru(n)
    nothing cool yet tho :)
    - cleaned up command:test

--- commands/test.js
+++ commands/test.js
@@ -12,8 +12,4 @@ exports.run = (bot, message) => {
 
     if (message.author.id != bot.config.kyuID) return; // limited to me
 
-    let output = message.author.username; // bot.annualEvent(9, 21, 9, 30).toString();
-
-    message.reply("you are **" + output + "** !!");
-
 };
